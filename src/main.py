f00f1542cbd92d94b252c3c90734936d2ff66187
Aziz Ur Rehman <urrehmanaziz379@gmail.com>
2023-10-12 21:57:58 +0200

    fixed issues

--- src/main.py
+++ src/main.py
@@ -4,13 +4,6 @@ import os
 import yaml
 
 
-# size = 228, 228
-
-# im= Image.open(r"C:\Users\urreh\Documents\MATH-OCR\MfrDB2934.png")
-# im.thumbnail(size)
-# im.convert('RGB')
-# array = np.array(im)
-
 if __name__=="__main__":
     config = yaml.safe_load(open(r".\config.yml"))
     directory = config['INKML_FILES_PATH']
